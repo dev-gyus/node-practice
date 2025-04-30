844707ace1295807b5ae4020d96e9635de8a106c
GyuHyeong <cjsworbehd13@gmail.com>
2025-04-30 15:41:34 +0900

    노드 소스 업데이트

--- chap3/func.js
+++ chap3/func.js
@@ -1,19 +1,5 @@
-// const value = require('./var');
-const {odd, even} = require('./var');
+const varObj = require('./var');
+const { odd, even } = require('./var');
 
-// console.log(value);
-
-function checkOddOrEven(number){
-    if(number % 2 ){
-        return odd;
-    }else{
-        return even;
-    }
-}
-
-// 다른 module에서 선언한 변수도 같이 module로 넘길 수 있다
-module.exports = {
-    checkOddOrEven,
-    odd,
-    even,
-}
+console.log(`odd = ${varObj.odd} / even = ${varObj.even}`);
+console.log(`odd = ${odd} / even = ${even}`);
